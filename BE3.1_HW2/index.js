9346d5655569ff4c2e770daef92ed2157e5f4ba0
Priyanka <priyanka.ss1@outlook.com>
2024-10-15 22:47:13 +0530

    add BE3.1_HW2

---
+++ BE3.1_HW2/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || PORT;
+
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from express server.');
+});
+
+app.get('/signin', (req, res) => {
+  res.send('This is the Sign In page.');
+});
+
+app.get('/booking', (req, res) => {
+  res.send('Book your tickets here.');
+});
+
+app.get('/clothing/kids', (req, res) => {
+  res.send('This is the kids wear page.');
+});
+
+app.get('/blog', (req, res) => {
+  res.send('This is the blog page.');
+});
